2bc819a304f1a517bbb3e23112b8bc450d004445
pumiWebdesigner <pumi.webdesigner@gmail.com>
2024-02-12 19:50:37 +0900

    contact修正

--- js/contact.js
+++ js/contact.js
@@ -1,28 +1,27 @@
 {
   // form全体
-  // const form = document.getElementById("js-form")
-  // form送信ボタン
-  // const submit = document.getElementById("js-submit1")
   const form = jQuery("#js-form");
   // form送信ボタン
   const submit = jQuery("#js-submit");
+  // 送信ボタンは初期値disabled
   submit.prop("disabled", true);
-
+  // const email = jQuery(".input-email");
+  // email.prop("invalid", false);
   form.submit(function () {
     $.ajax({
-      url: $form.attr("action"),
-      data: $form.serialize(),
+      url: form.attr("action"),
+      data: form.serialize(),
       type: "POST",
       dataType: "xml",
       statusCode: {
         0: function () {
           //送信に成功したときの処理
-          $form.slideUp();
+          form.slideUp();
           jQuery("#js-success").slideDown();
         },
         200: function () {
           //送信に失敗したときの処理
-          $form.slideUp();
+          form.slideUp();
           jQuery("#js-error").slideDown();
         },
       },
@@ -30,13 +29,26 @@
     return false;
   });
 
-  form.on("change", function () {
-    // jQueryは直接chedkValidity()をformに対して使用できない
-    // 対象のDOM要素を取得（.get(0)や[0]を使用してから）checkValidityを行う
-    if (form.get(0).checkValidity()) {
-      submit.prop("disabled", false);
-    } else {
-      submit.prop("disabled", true);
-    }
+  const requires = jQuery(".is-required");
+  requires.forEach(function (require) {
+    require.on("change", function () {
+      // jQueryは直接chedkValidity()をrepuiredに対して使用できない
+      // 対象のDOM要素を取得（.get(0)や[0]を使用してから）checkValidityを行う
+      if (require.checkValidity()) {
+        submit.prop("disabled", false);
+      } else {
+        submit.prop("disabled", true);
+      }
+    });
   });
+  // form.on("change", function () {
+  // form.on("input", function () {
+  //   // jQueryは直接chedkValidity()をformに対して使用できない
+  //   // 対象のDOM要素を取得（.get(0)や[0]を使用してから）checkValidityを行う
+  //   if (form.get(0).checkValidity()) {
+  //     submit.prop("disabled", false);
+  //   } else {
+  //     submit.prop("disabled", true);
+  //   }
+  // });
 }
